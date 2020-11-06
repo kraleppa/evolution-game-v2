ec0012bc40d8af473907ecc23b753a4206b9b32b
krzysztof <misiek.nalepa@wp.pl>
2020-11-06 12:37:57 +0100

    Entities added position parameter

--- src/main/java/io/github/kraleppa/entities/Animal.java
+++ src/main/java/io/github/kraleppa/entities/Animal.java
@@ -1,6 +1,16 @@
 package io.github.kraleppa.entities;
 
+import io.github.kraleppa.util.Vector2D;
+
 public class Animal extends Entity{
+    public Animal(Vector2D position) {
+        super(position);
+    }
+
+    public Animal() {
+        this(new Vector2D(0, 0));
+    }
+
     @Override
     public String toString() {
         return "#";
--- src/main/java/io/github/kraleppa/entities/Entity.java
+++ src/main/java/io/github/kraleppa/entities/Entity.java
@@ -1,4 +1,11 @@
 package io.github.kraleppa.entities;
 
+import io.github.kraleppa.util.Vector2D;
+
 public abstract class Entity {
+    public final Vector2D position;
+
+    public Entity(Vector2D position) {
+        this.position = position;
+    }
 }
