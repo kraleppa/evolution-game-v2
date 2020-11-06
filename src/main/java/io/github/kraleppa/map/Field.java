cd7b5370992d703acf82988f546542ac7de9067a
krzysztof <misiek.nalepa@wp.pl>
2020-11-06 12:11:28 +0100

    Field equals and hashcode added

--- src/main/java/io/github/kraleppa/map/Field.java
+++ src/main/java/io/github/kraleppa/map/Field.java
@@ -2,12 +2,22 @@ package io.github.kraleppa.map;
 
 import io.github.kraleppa.entities.Entity;
 import io.github.kraleppa.util.Vector2D;
+import lombok.EqualsAndHashCode;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
+@EqualsAndHashCode
 public class Field {
+    private final Vector2D position;
+
+    @EqualsAndHashCode.Exclude
     private final Set<Entity> entities = new HashSet<>();
+
+    public Field(Vector2D position) {
+        this.position = position;
+    }
 
     public void addEntity(Entity entity){
         entities.add(entity);
