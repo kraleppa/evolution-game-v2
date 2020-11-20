6552a11566902412151dc9e674d140c41e13e4a7
krzysztof <misiek.nalepa@wp.pl>
2020-11-20 14:40:49 +0100

    Genotype finished

--- src/main/java/io/github/kraleppa/animal/Genotype.java
+++ src/main/java/io/github/kraleppa/animal/Genotype.java
@@ -1,16 +1,44 @@
 package io.github.kraleppa.animal;
 
+import java.util.*;
+
 public class Genotype {
-    public final String genotype;
+    public final List<Integer> genotype = new ArrayList<>();
+    private final Random random = new Random();
 
     public Genotype() {
-        //TODO
-        this.genotype = null;
+        for (int i = 0; i < 32; i++){
+            genotype.add(i, random.nextInt(8));
+        }
+        addMissingGenes();
     }
 
     public Genotype(Genotype parent1, Genotype parent2){
-        //TODO
-        this.genotype = null;
+        for (int i = 0; i < 32; i++){
+            genotype.add(i % 2 == 0 ? parent1.genotype.get(i) : parent2.genotype.get(i));
+        }
+        addMissingGenes();
+    }
+
+    private void insertGene(int gene){
+        boolean[] genes = new boolean[8];
+        for (int i = 0; i < genotype.size(); i++){
+            if (genes[genotype.get(i)]){
+                genotype.set(i, gene);
+                break;
+            }
+            genes[genotype.get(i)] = true;
+        }
+    }
+
+
+    private void addMissingGenes(){
+        for (int i = 0; i < 8; i++){
+            if (!genotype.contains(i)){
+                insertGene(i);
+            }
+        }
+        Collections.sort(genotype);
     }
 
 
